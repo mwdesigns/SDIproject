4b4bd5bf0c985e61b4a10590dd0a8f50f71ad86b
Mike White <mikewhitedesigns@gmail.com>
2012-01-12 21:05:21 -0700

    first push of new progect 2 code

--- white_mike_assignment2.js
+++ white_mike_assignment2.js
@@ -1,7 +1,43 @@
 // author Mike White
 // Created on: January 11, 2012
-// Assignment: Git
+// Assignment: Project 2
 
 
 alert("JavaScript works!");
+
+
+var 
+	zombieHunter = "Columbus",
+	numZombies = 5,
+	bodyParts = ["arm","leg","chest","head" ],
+	zombieParts = function(bodyParts) {
+		bodyParts++;
+	};
+;
+
+
+
+console.log(
++ zombieHunter + " walked out of the convenient store just in time to see " + numZombies +  " zombies coming toward him. But he could remember if he was supposed to aim for the " + bodyParts + "."
+);
+
+console.log(
+"He pulled out his shot gun and shot for the " + zombieParts + "."
+);
+while (partsRemaining > 0) {};
+	console.log("Looks like I might have to try the " + bodyParts + " to kill them."
+);
+
+
+
+/*
+    Our new crewmember var crewName = "Wesley"; had only been at sea for var daysSailing = 8; days, but he was already rethinking his decision to become a pirate. All he had done so far was swab the decks = [ "Bridge", "Gun deck", "Main deck", "Quarterdeck" ];, which didn't feel very Pirate-y. It felt like his days were just the same thing over and over again:
+
+    Is it time to var swabTheDecks = function(decks){...};? It looks like I have var decksRemaining = decks.length; to go. The First Mate says I have to keep swabbing while (decksRemaining > 0), so I guess I'd better get to it.
+
+    And just like each day was the same, each deck was the same, so that for (var deckNumber = 0; deckNumber < decks.length; deckNumber++) every deck that he was told to do, he just swabbed and swabbed and swabbed. No matter who he complainTo = function(person,rant){...};, the result was always the same: return "Get back to work, " + crewName;.
+
+    In time, he kept his mind occupied by imagining what would happen if (ninjas.decideToAttack()) in the middle of the night.
+ 
+*/
 
