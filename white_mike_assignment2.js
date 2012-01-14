68187da2294251aa601c6978d1b80a9edb5ce5f8
Mike White <mikewhitedesigns@gmail.com>
2012-01-13 23:07:46 -0700

    finalized code, will turn in but I think I may be missing something

--- white_mike_assignment2.js
+++ white_mike_assignment2.js
@@ -5,39 +5,40 @@
 
 alert("JavaScript works!");
 
-
 var 
 	zombieHunter = "Columbus",
 	numZombies = 5,
 	bodyParts = ["arm","leg","chest","head" ],
-	zombieParts = function(bodyParts) {
-		bodyParts++;
-	};
+	bullets = 50
+	bulletsPerZombie = [2, 4, 6, 8]
+;
+var zombieParts = function () {
+	var bodyPart = bodyParts[partNumber];
+		bulletsRemaining = bulletsPerZombie[partNumber]
+		console.log("He pulled out his shot gun and shot for the " + bodyPart + ". " + "Remembering the double tap rule he thought, there goes " + bulletsRemaining + " bullets");
+};
+var zombieDown = 1,
+	zombieToGo = numZombies - 1
 ;
 
 
 
+// here's the story...
 console.log(
-+ zombieHunter + " walked out of the convenient store just in time to see " + numZombies +  " zombies coming toward him. But he could remember if he was supposed to aim for the " + bodyParts + "."
+zombieHunter + " walked out of the convenient store just in time to see " + numZombies +  " zombies coming toward him. But he could remember if he was supposed to aim for the " + bodyParts + "."
 );
 
-console.log(
-"He pulled out his shot gun and shot for the " + zombieParts + "."
-);
-while (partsRemaining > 0) {};
-	console.log("Looks like I might have to try the " + bodyParts + " to kill them."
-);
+for (var partNumber = 0; partNumber < bodyParts.length; partNumber++) {
+	zombieParts()
+}
 
+while (numZombies > 0) {
+	console.log("There's " + zombieDown + " zombies down and " + zombieToGo + " zombies to go.");
+	numZombies--;
+	zombieDown++;
+	zombieToGo--;
+	
+};
+console.log("Take That! No more zombies.");
 
-
-/*
-    Our new crewmember var crewName = "Wesley"; had only been at sea for var daysSailing = 8; days, but he was already rethinking his decision to become a pirate. All he had done so far was swab the decks = [ "Bridge", "Gun deck", "Main deck", "Quarterdeck" ];, which didn't feel very Pirate-y. It felt like his days were just the same thing over and over again:
-
-    Is it time to var swabTheDecks = function(decks){...};? It looks like I have var decksRemaining = decks.length; to go. The First Mate says I have to keep swabbing while (decksRemaining > 0), so I guess I'd better get to it.
-
-    And just like each day was the same, each deck was the same, so that for (var deckNumber = 0; deckNumber < decks.length; deckNumber++) every deck that he was told to do, he just swabbed and swabbed and swabbed. No matter who he complainTo = function(person,rant){...};, the result was always the same: return "Get back to work, " + crewName;.
-
-    In time, he kept his mind occupied by imagining what would happen if (ninjas.decideToAttack()) in the middle of the night.
- 
-*/
 
