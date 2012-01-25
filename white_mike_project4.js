b081f4376c194af71b8e812b126e92cbf04e5d18
Mike White <mikewhitedesigns@gmail.com>
2012-01-25 13:13:00 -0700

    added form field in html to test email validator

--- white_mike_project4.js
+++ white_mike_project4.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+// author Mike White
+// Created on: January 23, 2012
+// Assignment: Project 4
+
+// alert("JavaScript works!");
+// //// STRING SETS ////
+
+// phone number pattern - add hyphens
+/*var phoneNum = function(){}
+var phoneNumAreaCode = 
+var phoneNumPrefix = 
+var phoneNumAreaCode = 
+*/
+// email validator - we use this one at work all the time
+function validEmail (field, alertTxt) 
+{
+	with (field)
+	{
+		apos = value.indexOf("@");
+		dotpos = value.lastIndexOf(".");
+			if(apos<1 || dotpos-apos<2)
+				{alert("Please enter a valid email"); return false;}
+			else
+				{ return true; }
+	}
+}
+
