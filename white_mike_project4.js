3a3b412e33edac3dab9837c2b0f27bc3c6cdadad
Mike White <mikewhitedesigns@gmail.com>
2012-01-27 13:03:30 -0700

    added TitleCase function

--- white_mike_project4.js
+++ white_mike_project4.js
@@ -17,20 +17,33 @@ var getPhoneNum = function(nStr)
 	}
 	return x1;
 };
-console.log(getPhoneNum("5555555555"));
+//console.log(getPhoneNum("5555555555"));
+// END phone number pattern
 
 // email validator - we use something similar to this at work all the time
 var validEmail = function(field, alertTxt) 
 {
-	var x=document.forms["emailForm"]["email"].value;
-	var atpos=x.indexOf("@");
-	var dotpos=x.lastIndexOf(".");
+	var x = document.forms["emailForm"]["email"].value;
+	var atpos = x.indexOf("@");
+	var dotpos = x.lastIndexOf(".");
 	if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
 	{
 		alert("Please enter a valid e-mail address");
 		return false;
 	}
+};
+//END email validator
+
+//Is the string a URL? (Does it start with http: or https:?)
+//[ToDo] create this function
+
+// Title Case String - returns the first letter of each word to capital
+var toTitleCase = function(toTransform) {
+  return toTransform.replace(/\b([a-z])/g, function (_, initial) {
+      return initial.toUpperCase();
+  });
 }
+//console.log(toTitleCase("mike d. white"))
 
 // **** NUMBER SETS **** //
 /* formats a number to 2 decimal places
